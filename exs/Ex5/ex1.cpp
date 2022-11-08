fcade0fd0d38d4d8d52db196ac704f382e35109b
minesamaro <22.maria.amaro@gmail.com>
2022-11-08 15:37:36 +0000

    Ex 51 is correct

--- exs/Ex5/ex1.cpp
+++ exs/Ex5/ex1.cpp
@@ -6,16 +6,16 @@ using namespace std;
 char encryptUppChar(char c, int key)
 {
     // assuming uppercase
-    if (key < 0) key = 'Z'-'A' + key;
-    char end = 'A' +  (c + key- 'A')%('Z' -'A');
+    if (key < 0) key = 'Z'-'A' +1 + key;
+    char end = 'A' +  (c + key- 'A')%('Z' -'A' + 1);
     return end; 
 
 }
 char encryptLowChar(char c, int key)
 {
     // assuming lowercase
-    if (key < 0) key = 'z'-'a' + key;
-    char end = 'a' +  (c + key- 'a')%('z' -'a');
+    if (key < 0) key = 'z'-'a' +1  + key;
+    char end = 'a' +  (c + key- 'a')%('z' -'a' +1);
     return end; 
 
 }
@@ -35,7 +35,8 @@ string encryptString(string s, int key)
 
 int main()
 {
+    //string s = "Dro aesmu lbygx pyh TEWZC yfob dro vkji nyq";
     string s = "The quick brown fox JUMPS over the lazy dog";
-    int key = 10;
-    cout << encryptString(s, key);
+    int key = -10;
+    cout << encryptString(s, key) << endl;
 }
